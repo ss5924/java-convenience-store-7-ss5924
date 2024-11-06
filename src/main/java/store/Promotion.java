e2a531a2858b4b28ea1431b07b662062ac00d086
songhashin <shsha9292@gmail.com>
2024-11-06 18:27:27 +0900

    feat (Promotion) : Promotion 도메인 구현

---
+++ src/main/java/store/Promotion.java
@@ -0,0 +1,35 @@
+package store;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private final String name;
+    private final int requiredCondition;
+    private final int giftQuantity;
+    private final LocalDateTime startAt;
+    private final LocalDateTime endAt;
+
+    public Promotion(String name, int requiredCondition, int giftQuantity, LocalDateTime startAt, LocalDateTime endAt) {
+        validate(requiredCondition, giftQuantity, startAt, endAt);
+        this.name = name;
+        this.requiredCondition = requiredCondition;
+        this.giftQuantity = giftQuantity;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    private void validate(int requiredCondition, int giftQuantity, LocalDateTime startAt, LocalDateTime endAt) {
+        if (requiredCondition <= 0) {
+            throw new IllegalArgumentException("[ERROR] 프로모션 참여 구매 개수 조건은 0을 초과해야 합니다.");
+        }
+
+        if (giftQuantity < 0) {
+            throw new IllegalArgumentException("[ERROR] 프로모션 증정품 개수는 0이상이어야 합니다.");
+        }
+
+        if (startAt.isAfter(endAt)) {
+            throw new IllegalArgumentException("[ERROR] 프로모션 종료일이 시작일보다 빠릅니다.");
+        }
+    }
+
+}
---
+++ src/test/java/store/PromotionTest.java
@@ -0,0 +1,56 @@
+package store;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PromotionTest {
+    private LocalDateTime baseDate;
+    private LocalDateTime now;
+
+    @BeforeEach
+    void setUp() {
+        baseDate = LocalDateTime.of(2024, 11, 5, 23, 59, 59);
+        now = DateTimes.now();
+    }
+
+    @Test
+    void 프로모션_구매_참여조건은_0이하면_예외가_발생한다() {
+        LocalDateTime startAt = baseDate;
+        LocalDateTime endAt = startAt.plusDays(1);
+
+        assertDoesNotThrow(() -> new Promotion("프로모션명", 1, 1, startAt, endAt));
+
+        assertThatThrownBy(() -> new Promotion("프로모션명", 0, 1, startAt, endAt))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션_증정품개수는_0미만이면_예외가_발생한다() {
+        LocalDateTime startAt = baseDate;
+        LocalDateTime endAt = startAt.plusDays(1);
+
+        assertDoesNotThrow(() -> new Promotion("프로모션명", 1, 0, startAt, endAt));
+
+        assertThatThrownBy(() -> new Promotion("프로모션명", 1, -1, startAt, endAt))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션_종료일이_시작일보다_빠르면_예외가_발생한다() {
+        LocalDateTime startAt = baseDate;
+        LocalDateTime endAt = startAt.minusDays(1);
+
+        assertThatThrownBy(() -> new Promotion("프로모션명", 1, -1, startAt, startAt))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Promotion("프로모션명", 1, -1, startAt, endAt))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
