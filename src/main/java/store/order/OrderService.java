edfe585f085d81cebe3c9d89dbb178e8e33445fc
songhashin <shsha9292@gmail.com>
2024-11-10 06:59:22 +0900

    feat : orderService 개발

---
+++ src/main/java/store/order/OrderService.java
@@ -0,0 +1,31 @@
+package store.order;
+
+import store.io.InputToOrderConverter;
+import store.product.InventoryReadService;
+
+import java.time.LocalDateTime;
+
+public class OrderService {
+    private final InventoryReadService inventoryReadService;
+    private final InputToOrderConverter inputToOrderConverter;
+
+    public OrderService(InventoryReadService inventoryReadService, InputToOrderConverter inputToOrderConverter) {
+        this.inventoryReadService = inventoryReadService;
+        this.inputToOrderConverter = inputToOrderConverter;
+    }
+
+    public Order createOrder(String input, LocalDateTime now) {
+        Order order = inputToOrderConverter.convertToOrder(input);
+        order.getOrderItems().forEach(orderItem -> {
+            int availableStock = inventoryReadService.getStockByProduct(orderItem.getProduct(), now);
+            validateStockAvailability(orderItem.getQuantity(), availableStock);
+        });
+        return order;
+    }
+
+    public void validateStockAvailability(int requestedQuantity, int availableStock) {
+        if (requestedQuantity > availableStock) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
+    }
+}
